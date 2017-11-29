fde2f2a0a234f08365659397c3a0c0189541e64d
Zac-L <Zachlichens@gmail.com>
2017-11-29 11:01:38 -0800

    get by id and post methods are now green

--- server/lib/routes/categories.js
+++ server/lib/routes/categories.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-// const bodyParser = require('body-parser').json();
+const bodyParser = require('body-parser').json();
 const Category = require('../models/category');
 
 router
@@ -9,6 +9,27 @@ router
             .lean()
             .then(categories => res.send(categories))
             .catch(next);
-    });
+    })
+
+    .get('/:id', (req, res, next) => {
+        Category.findById(req.params.id)
+            .lean()
+            .then( category => {
+                if(!category) {
+                    throw { code: 404, error: `id ${req.params.id} does not exist` };
+                }
+                else res.json(category);
+            })
+            .catch(next);
+
+    })
+
+    .post('/', bodyParser, (req, res, next) => {
+        new Category(req.body)
+            .save()
+            .then(saved => res.send(saved))
+            .catch(next);
+    })
+;
 
 module.exports = router;
