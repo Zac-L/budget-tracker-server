d46f063ab1e972005eea3726a2db96769ce0e35e
Zac-L <Zachlichens@gmail.com>
2017-11-29 11:01:52 -0800

    created test for post and get by id methods. test is now passing

--- server/test/categories.api.test.js
+++ server/test/categories.api.test.js
@@ -12,8 +12,37 @@ describe('categories', () => {
     before(db.drop(connection));
     const request = chai.request(app);
 
+    let category = { name: 'Zach', budget: '100' };
+
     it('/GET all', () => request
         .get('/api/categories')
         .then(res => assert.deepEqual(res.body, []))
+    );
+
+    it('/GET by id', () => {
+        let categories = null;
+        return request.post('/api/categories')
+            .send(category)
+            .then(res => {
+                categories = res.body;
+                return request.get(`/api/categories/${categories._id}`);
+            })
+            .then(res => {
+                assert.deepEqual(res.body, categories);
+            });
+    });
+
+    // it('/GET returns 404 for bad id given', () => {
+
+    // })
+
+    it('POST', () => request 
+        .post('/api/categories')
+        .send(category)
+        .then(({ body }) => {
+            assert.ok(body._id);
+            assert.equal(category.name, body.name);
+            category = body;
+        })
     );
 });
