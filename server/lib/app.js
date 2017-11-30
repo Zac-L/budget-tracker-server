ea044a530eed9b7f6a896e7cd92080ec3672693c
Zac-L <Zachlichens@gmail.com>
2017-11-29 23:39:37 -0800

    move some code around, cleaned it up a bit, and removed some outdated code

--- server/lib/app.js
+++ server/lib/app.js
@@ -1,30 +1,31 @@
 const express = require('express');
 const app = express();
-
-/* middleware */
+const errorHandler = require('./error-handler')();
 const morgan = require('morgan');
 const redirectHttp = require('./redirect-http')();
 const cors = require('cors')();
 const checkDb = require('./check-connection')();
-const errorHandler = require('./error-handler')();
 
+// ### Middleware ###
+app.use(express.json());
 app.use(morgan('dev'));
 app.use(cors);
+app.use(express.static('./public'));
 
 
-// Redirect http to https in production
+// ### Redirect http to https in production ###
 if(process.env.NODE_ENV === 'production') {
     app.use(redirectHttp);
 }
-app.use(express.static('./public'));
 
-/* routes */
+// ### Required Routes ###
 const category = require('./routes/categories');
 
-
+// ## Used Routes ###
 app.use(checkDb);
 app.use('/api/categories', category);
 
+// ### Catchers ###
 app.use(errorHandler);
 
 module.exports = app;
