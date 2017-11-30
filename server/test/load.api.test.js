5ceac9da3130fe67e2edcb6af0f2514be56a4280
Zac-L <Zachlichens@gmail.com>
2017-11-30 12:14:56 -0800

    green test for simulated wait response

--- server/test/load.api.test.js
+++ server/test/load.api.test.js
@@ -12,4 +12,12 @@ describe('test route for simulated errors and loading', () => {
             assert.deepEqual(res.body, { answer: 'vanilla response' });
         })
     );
+
+    // TODO: need to figure out how to dynamically account for wait time.
+    it('/GET wait response', () => request
+        .get('/api/test?wait=1500')
+        .then(res => {
+            assert.deepEqual(res.body, { answer: 'waited for 1500ms' });
+        })
+    );
 });
