d56f015ee903d17d81472ac3f5ab33b7848197ab
Zac-L <Zachlichens@gmail.com>
2017-11-29 23:39:07 -0800

    removed some unneeded code, updated to work better

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-const app = require('./server/lib/app');
 const http = require('http');
-const port = process.env.PORT || 3001;
-require('./server/lib/setup-mongoose');
+const app = require('./server/lib/app');
+const connect = require('./server/lib/connect');
+
+connect();
 
 const server = http.createServer(app);
+const port = process.env.PORT || 3001;
+
 server.listen(port, () => {
-    console.log('server running at', server.address());
+    // eslint-disable-next-line
+    console.log('server running on', server.address().port);
 });
